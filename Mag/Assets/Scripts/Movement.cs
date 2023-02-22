9813ad3cd994b29530ff8d7465570bde91fe0ad8
AltoHinigo <VBA2002@yandex.ru>
2023-02-22 23:55:20 +0300

    Improvement of movement mechanics
    The character looks at the cursor.

---
+++ Mag/Assets/Scripts/Movement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
+
+public class Movement : MonoBehaviour
+{
+    public Camera MCamera;
+
+    private RaycastHit Hit;
+
+    private NavMeshAgent Agent;
+
+    private string GroundTag = "Graund";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 newDir = Vector3.RotateTowards(transform.forward, (target.transform.position - transform.position), radian_angle, 0.0F);
+        //transform.rotation = Quaternion.LookRotation(newDir);
+        Ray ray = MCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out Hit, Mathf.Infinity))
+            if (Hit.collider.CompareTag(GroundTag))
+            {
+                var direction = (Hit.point - transform.position).normalized;
+                direction.y = 0f;
+                transform.rotation = Quaternion.LookRotation(direction);
+                if (Input.GetMouseButton(0))
+                    Agent.SetDestination(Hit.point);
+            }
+    }
+    /*
+    void LookAtXZ(this Transform transform, Vector3 point)
+    {
+        var direction = (point - transform.position).normalized;
+        direction.y = 0f;
+        transform.rotation = Quaternion.LookRotation(direction);
+    }
+
+    void LookAtXZ(this Transform transform, Vector3 point, float speed)
+    {
+        var direction = (point - transform.position).normalized;
+        direction.y = 0f;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(direction), speed);
+    }*/
+}
+
---
+++ Mag/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [Header("Move speed")]
+    public float speed = 7f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        //transform.localPosition += transform.forward * speed * Time.deltaTime;
+    }
+
+    private void Move()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.localPosition += transform.forward * speed * Time.deltaTime;
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.localPosition += transform.right * speed * Time.deltaTime;
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.localPosition -= transform.forward * speed * Time.deltaTime;
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.localPosition -= transform.right * speed * Time.deltaTime;
+
+        }
+    }
+}

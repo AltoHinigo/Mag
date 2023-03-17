d03c52b0b06edd1ff3fe17aa2b9782e3076dc023
AltoHinigo <VBA2002@yandex.ru>
2023-03-18 00:27:52 +0300

    Сhanged graphics engine
    3D(HDRP) -> 3D
    Added new modals

--- Mag/Assets/Scripts/Movement.cs
+++ Mag/Assets/Scripts/Movement.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Timers;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UIElements;
@@ -9,21 +11,40 @@ public class Movement : MonoBehaviour
 {
     public Camera MCamera;
 
+    public GameObject ParticleSystemObject;//var
+
+    private ParticleSystem aParticleSystem;//var
+
     private RaycastHit Hit;
 
     private NavMeshAgent Agent;
 
-    private string GroundTag = "Graund";
+    private string GroundTag = "Ground";
 
     private Animator animator;
 
-    // Start is called before the first frame update
+    private bool TimeIsOver = false;
+
+    private int[] Elements = new int[5];
+
+    private static System.Timers.Timer aTimer;
     void Start()
     {
+        aParticleSystem = ParticleSystemObject.GetComponent<ParticleSystem>();//var
         Agent = GetComponent<NavMeshAgent>();
+        aTimer = new System.Timers.Timer(2000);
+        aTimer.Elapsed += OnTimer;
+        aTimer.AutoReset = false;
     }
 
-    private void Awake()
+    private void OnTimer(object s, ElapsedEventArgs e)
+    {
+        foreach (ref int element in Elements.AsSpan())
+            element = 0;
+        TimeIsOver = true;
+    }
+
+    void Awake()
     {
         animator = GetComponent<Animator>();
     }
@@ -31,28 +52,60 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Vector3 newDir = Vector3.RotateTowards(transform.forward, (target.transform.position - transform.position), radian_angle, 0.0F);
-        //transform.rotation = Quaternion.LookRotation(newDir);
+        Debug.LogWarningFormat("Play");
         Ray ray = MCamera.ScreenPointToRay(Input.mousePosition);
-            if(Physics.Raycast(ray.origin, ray.direction, out Hit))//if (Physics.Raycast(ray, out Hit, Mathf.Infinity))
-            if (Hit.collider.CompareTag(GroundTag))
+        var direction = (Hit.point - transform.position).normalized;
+        direction.y = 0f;
+        transform.rotation = Quaternion.LookRotation(direction);
+        if (Input.GetKey(KeyCode.W))
+            Elements[0] = 1;
+        if (Physics.Raycast(ray.origin, ray.direction, out Hit, Mathf.Infinity, 1 << 7))//if (Physics.Raycast(ray, out Hit, Mathf.Infinity))
+            if (Elements[0] == 0)
             {
-                var direction = (Hit.point - transform.position).normalized;
-                direction.y = 0f;
-                transform.rotation = Quaternion.LookRotation(direction);
-                if (Input.GetMouseButton(0))
-                    Agent.SetDestination(Hit.point);
+                if (TimeIsOver)
+                {
+                    aParticleSystem.Pause();
+                    aParticleSystem.Clear();
+                }
+                if (Hit.collider.CompareTag(GroundTag))
+                {
+                    if (Input.GetMouseButton(0))
+                        Agent.SetDestination(Hit.point);
 
 
-                
-                if(Agent.hasPath)
-                {
-                    animator.SetFloat("Speed", 0.5f);
+
+                    if (Agent.hasPath)
+                        animator.SetBool("Walk", true);
+                    else
+                        animator.SetBool("Walk", false);
                 }
-                else
+            }
+            else
+            {
+                if (!aTimer.Enabled)
                 {
-                    animator.SetFloat("Speed", 0f);
+                    //Debug.LogWarningFormat("Play");
+                    //Debug.LogWarningFormat("W = {0}", Input.GetKey(KeyCode.W));
+                    //Debug.LogWarningFormat("!Elements[0] = {0}", Elements[0]);
+                    aTimer.Start();
+                    aParticleSystem.Play();//var
+                    animator.SetBool("Walk", false);
+                    Agent.ResetPath();
                 }
+                if (Hit.collider.CompareTag(GroundTag))
+                {
+                    
+
+                    if (Input.GetMouseButton(0))
+                    {
+                        Agent.SetDestination(Hit.point);
+                        //foreach (int i in Elements)
+                        //    Elements[i] = 0;
+                        foreach (ref int element in Elements.AsSpan())
+                            element = 0;
+                    }
+                }
+
             }
     }
     /*
